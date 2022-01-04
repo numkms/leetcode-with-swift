527e25e985aff4b41d00ce7489974b06579a275b
numawsm <numkms@gmail.com>
2022-01-04 11:20:50 +0200

    atoi (not so ez)

---
+++ atoi.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+     //32bit bound
+     let bound32bit = 2**31
+     // allowed symbols
+     let allowed = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
+     // increment
+     let i = 0
+     // result
+     let result = ""
+     // sign (minus or plus)
+     let sign = 1
+     
+     // skip whitespace
+     while(s[i] == " ") { i++ }
+     
+     // determine sign (plus or minus)
+     if(s[i] == "-" || s[i] == "+") {
+         sign = s[i] == "+" ? 1 : -1 
+         i++
+     }   
+     
+     // build result while its numbers 
+     while(allowed.includes(s[i])) {
+         result += s[i]
+         i++
+     }
+     // apply sign to result
+     result = result * sign
+    
+     // return result with 32bit bounds 
+     return result > 0 ? Math.min(result, bound32bit -1) : Math.max(result, -bound32bit)
+};
